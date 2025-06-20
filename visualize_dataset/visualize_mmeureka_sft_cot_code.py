090f5bad0b2e4d9cec3312eb5ba3989b00650d95
AutomanHan <hancong0911@163.com>
2025-06-20 17:48:00 +0800

    add mac data

--- visualize_dataset/visualize_mmeureka_sft_cot_code.py
+++ visualize_dataset/visualize_mmeureka_sft_cot_code.py
@@ -8,11 +8,20 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 # 正确配置图片服务路由
-@app.route('/dolphinfs/<path:subpath>')
-def serve_dolphinfs(subpath):
-    """正确映射/mnt/dolphinfs到web路径"""
-    base_dir = "/mnt/dolphinfs"
-    return send_from_directory(base_dir, subpath)
+# @app.route('/nathan/<path:subpath>')
+# def serve_dolphinfs(subpath):
+#     """正确映射/mnt/dolphinfs到web路径"""
+#     base_dir = "/User/nathan"
+#     return send_from_directory(base_dir, subpath)
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    base_dir = '/Users/nathan/projects'  # 本地存储根目录
+    return send_from_directory(
+        # os.path.join(base_dir, 'data/retool/FanqingM/MM-Eureka-Dataset'), 
+        base_dir,
+        filename
+    )
+
 
 class DataLoader:
     def __init__(self, data_path, data_path_cotcode, data_path_sft):
@@ -21,17 +30,28 @@ class DataLoader:
         self.data_path_sft = data_path_sft
         self.data = []
         self.data_cotcode = []
-        self.data_cocode_sft = []
+        self.data_cotcode_sft = []
         self.data_dict = {}
         self.image_root = "/Users/nathan/projects/data/retool/FanqingM/MM-Eureka-Dataset/inspire/hdd/global_user/shaowenqi-shaowenqi/mengfanqing/OpenRLHF-InternVL/dataset/report_data"
-        # self._load_data()
+        self._load_data()
         self._load_data_sft()
 
     def _load_data_sft(self):
+        old_root = "/mnt/dolphinfs/hdd_pool/docker/user/hadoop-basecv/hancong/code/pretrain/reasoning/data/MM-Eureka-Dataset/MMPR"
+        new_root = "/Users/nathan/projects/data/retool/FanqingM/MM-Eureka-Dataset"
         with open(self.data_path_sft, 'r', encoding='utf-8') as f:
-            import pdb;pdb.set_trace()
-            for line in f:
-                item = json.loads(line)
+            all_infos = json.load(f)
+            for item in all_infos:
+                images = [item["images"][0].replace(old_root, new_root)]
+                item["images"] = images
+                conversations = item["conversations"]
+                for conver in conversations:
+                    if conver["from"] == "user":
+                        item["question"] = conver["content"].replace('<', '&lt;').replace('>', '&gt;')
+                    if conver["from"] == "assistant":
+                        item["answer"] = conver["content"].replace('<', '&lt;').replace('>', '&gt;')
+                item["cot_code"] = "test"
+                self.data_cotcode_sft.append(item)
                 
     
     def _load_data(self):
@@ -58,29 +78,37 @@ class DataLoader:
                     #     images.append(web_path)
                 for image_file in item["image_urls"]:
                     # 转换路径为web访问格式
+                    # import pdb;pdb.set_trace()
                     image = os.path.join(self.image_root, image_file)
-                    web_path = image.replace(
-                        "/mnt/dolphinfs/", 
-                        "/dolphinfs/"
-                    )
+                    # web_path = image.replace("/Users/nathan","nathan")
+                    web_path = image.replace("/Users/nathan/projects","images")
                     images.append(web_path)
                     # images.append(content['image'])
                 # images.append(content['image'])
                 item['images'] = images
-                item["image_path"] = images[0].replace("/dolphinfs/","/mnt/dolphinfs/")
+                item["image_path"] = "/Users/nathan/projects"+images[0]
                 self.data.append(item)
                 self.data_dict[item["id"]] = item
         with open(self.data_path_cotcode, 'r', encoding='utf-8') as f:
-            for line in f:
-                item_code = json.loads(line)
-                math_id = item_code["id"]
+            all_infos = json.load(f)
+            for line in all_infos:
+                # item_code = json.loads(line)
+                item_code = line
+                math_id = item_code["id"].replace("mm_eureka_","")
                 # import pdb;pdb.set_trace()
                 if math_id in self.data_dict:
                     # 解析message字段获取图片路径
                     images = []
                     item = self.data_dict[math_id]
-                    item["cot_code"] = item_code["ans"].replace('<', '&lt;').replace('>', '&gt;')
-                    item["model"]= item_code["model"]
+                    # import pdb;pdb.set_trace()
+                    conversations = item_code["conversations"]
+                    for conver in conversations:
+                        # if conver["role"] == "user":
+                        #     item["question"] = conver["content"].replace('<', '&lt;').replace('>', '&gt;')
+                        if conver["from"] == "assistant":
+                            item["cot_code"] = conver["content"].replace('<', '&lt;').replace('>', '&gt;')
+                    # item["cot_code"] = item_code["ans"].replace('<', '&lt;').replace('>', '&gt;')
+                    item["model"]= item_code["answer_format"]
 
                     keep_dataset=["geoqa_plus","K12"]
                     need_skip = True
@@ -104,12 +132,20 @@ class DataLoader:
         end = start + per_page
         if random_sample:
             return random.sample(self.data_cotcode, min(per_page, len(self.data_cotcode)))
+        # import pdb;pdb.set_trace()
         return self.data_cotcode[start:end]
+    def get_page_cotcode_sft(self, page=0, per_page=100, random_sample=False):
+        start = page * per_page
+        end = start + per_page
+        if random_sample:
+            return random.sample(self.data_cotcode_sft , min(per_page, len(self.data_cotcode_sft)))
+        return self.data_cotcode_sft[start:end]
 
 # 初始化数据加载器
-data_jsonl="/mnt/dolphinfs/hdd_pool/docker/user/hadoop-basecv/hancong/code/pretrain/reasoning/data/MM-Eureka-Dataset/dataset.jsonl"
+data_jsonl="/Users/nathan/projects/data/retool/FanqingM/MM-Eureka-Dataset/dataset.jsonl"
 data_cotcode_jsonl = "/mnt/dolphinfs/hdd_pool/docker/user/hadoop-basecv/hancong/code/pretrain/reasoning/data/MM-Eureka-Dataset/synth_gemini_cot_code/dataset_20k_cot_code.jsonl"
 data_cotcode_jsonl="/mnt/dolphinfs/hdd_pool/docker/user/hadoop-basecv/hancong/code/pretrain/reasoning/data/MM-Eureka-Dataset/synth_gemini_cot_code/dataset_55k_cot_code_promptv3.jsonl"
+data_cotcode_jsonl = "/Users/nathan/projects/data/retool/MM_retool_sft_rl_data/SFT_CoT_Data/data_sft_cot_promptv3_15k_k12_geoqaplus.json"
 data_cotcode_sft = "/Users/nathan/projects/data/retool/MM_retool_sft_rl_data/SFT_CoT_Data/data_sft_cot_promptv3_15k_k12_geoqaplus.json"
 data_loader = DataLoader(data_jsonl, data_cotcode_jsonl, data_cotcode_sft)
 
@@ -121,10 +157,11 @@ def index():
 @app.route('/api/data')
 def get_data():
     page = int(request.args.get('page', 0))
-    per_page = int(request.args.get('per_page', 100))
+    per_page = int(request.args.get('per_page', 50))
     random_sample = request.args.get('random', 'false') == 'true'
     
     data = data_loader.get_page_cotcode(page, per_page, random_sample)
+    # import pdb;pdb.set_trace()
     return jsonify({
         'data': data,
         'total': len(data_loader.data_cotcode),
@@ -133,6 +170,6 @@ def get_data():
     })
 
 if __name__ == '__main__':
-    ip_host = "10.138.131.43"
+    ip_host = "127.0.0.1"
     port = 8419
     app.run(host=ip_host, port=port, debug=True)
